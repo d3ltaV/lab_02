c13c2f21940110defc274ead476cc138f9f51ad8
BuildTools <unconfigured@null.spigotmc.org>
2025-09-09 09:53:58 -0400

    make restaurant and category classes

--- src/category.py
+++ src/category.py
@@ -1,4 +1,43 @@
+from menuitems import MenuItem
+from typing import List
 class Category:
-    def __init__(self, id: int, category: str):
+    def __init__(self, id: int, category: str, items: List[MenuItem]):
         self.id = id
         self.category = category
+        self.items = items
+    def describe(self):
+        """
+        Describe this category
+        Parameters: None
+        Returns: None
+        """
+        print(f"Here is a list of items in category {self.category}!")
+        for i in range(len(self.items)):
+            print(f"{self.items[i]}: ")
+
+    def addItem(self, item: MenuItem, index: int):
+        """
+        Adds item to category
+        Parameters: 
+            item (MenuItem): item to add
+            index (int): index to add to
+        Returns: Success message (string)
+        """
+        self.menu.insert(index, item)
+        ret_msg = f"Added category {item.name} at index {index}!"
+        return ret_msg
+
+    def deleteItem(self, item: MenuItem):
+        """
+        Deletes category in restaurant
+        Parameters: 
+            item (MenuItem): item to delete
+        Returns: Success message (string) or error message (string)
+        """
+        if item in self.items:
+            self.menu.remove(item)
+            ret_msg = f"Removed category: {item.name}"
+            return ret_msg
+    
+        ret_msg = f"Category {item.name} not found!"
+        return ret_msg
